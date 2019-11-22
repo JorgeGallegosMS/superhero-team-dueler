276b903c9c029652387c474e7f3e66b76d955dc8
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-11-22 15:15:01 -0800

    Added Hero class and defined add_ability, and add_armor methods

--- superheroes.py
+++ superheroes.py
@@ -17,10 +17,29 @@ class Armor:
     def block(self):
         random_value = random.randint(0, self.max_block)
         return random_value
-        
+
+class Hero:
+    def __init__(self, name, starting_health=100):
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+
+        self.abilities = list()
+        self.armors = list()
+
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+
 if __name__ == "__main__":
     # If you run this file from the terminal
-    # this block is executed.
-    armor = Armor("Debugging Shield", 10)
-    print(armor.name)
-    print(armor.block())
+    # this block of code is executed.
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
