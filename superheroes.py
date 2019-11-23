38cfffbc53326e0474680d6abc5e60d8105722b1
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-11-22 18:13:21 -0800

    Added Team class

--- superheroes.py
+++ superheroes.py
@@ -91,6 +91,24 @@ class Hero:
                 else:
                     print(f"{self.name} wins!")
 
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.heroes = list()
+
+    def remove_hero(self, name):
+        
+        foundHero = False
+
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+
+                foundHero = True
+
+        if not foundHero:
+            return 0
+
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
